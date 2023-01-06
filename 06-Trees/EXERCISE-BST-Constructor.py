93fdc32a566f1c3dfe857f9b22d7ee102ca21a1d
Andy Villegas <andyvillegas440@gmail.com>
2023-01-05 22:31:28 -0500

    finish trees section

---
+++ 06-Trees/EXERCISE-BST-Constructor.py
@@ -0,0 +1,26 @@
+class Node:
+    def __init__(self, value) -> None:
+        self.value = value
+        self.rigth = None
+        self.left = None
+        
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+
+
+
+
+my_tree = BinarySearchTree()
+
+print(my_tree.root)
+
+
+ 
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    None
+
+"""
