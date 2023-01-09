01bfb09d61c78356c012f8d2c70ecf9862d9b7a9
Andy Villegas <andyvillegas440@gmail.com>
2023-01-08 21:21:02 -0500

    finish graphs section

---
+++ 08-Graphs/EXERCISE-GR-Add_Edge.py
@@ -0,0 +1,43 @@
+class Graph:
+    def __init__(self):
+        self.adj_list = {}
+
+    def print_graph(self):
+        for vertex in self.adj_list:
+            print(vertex, ':', self.adj_list[vertex])
+
+    def add_vertex(self, vertex):
+        if vertex not in self.adj_list.keys():
+            self.adj_list[vertex] = []
+            return True
+        return False
+
+    def add_edge(self, a, b):
+        vertexes = self.adj_list.keys()
+        if a in vertexes and b in vertexes:
+            self.adj_list[a].append(b)
+            self.adj_list[b].append(a)
+            return True
+        return False
+
+
+
+
+my_graph = Graph()
+
+my_graph.add_vertex(1)
+my_graph.add_vertex(2)
+
+my_graph.add_edge(1,2)
+
+my_graph.print_graph()
+
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    1 : [2]
+    2 : [1]
+
+"""
