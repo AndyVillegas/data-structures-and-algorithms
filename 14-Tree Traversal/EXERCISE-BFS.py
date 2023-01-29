389d0863c20125cc22fd5ad614a5238693f9ac58
Andy Villegas <andyvillegas440@gmail.com>
2023-01-29 14:46:16 -0500

    finish tree traversal section

---
+++ 14-Tree Traversal/EXERCISE-BFS.py
@@ -0,0 +1,90 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+        
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, value):
+        new_node = Node(value)
+        if self.root is None:
+            self.root = new_node
+            return True
+        temp = self.root
+        while (True):
+            if new_node.value == temp.value:
+                return False
+            if new_node.value < temp.value:
+                if temp.left is None:
+                    temp.left = new_node
+                    return True
+                temp = temp.left
+            else: 
+                if temp.right is None:
+                    temp.right = new_node
+                    return True
+                temp = temp.right
+
+    def contains(self, value):
+        if self.root is None:
+            return False
+        temp = self.root
+        while (temp):
+            if value < temp.value:
+                temp = temp.left
+            elif value > temp.value:
+                temp = temp.right
+            else:
+                return True
+        return False
+  
+    def BFS(self):
+        queue = [self.root]
+        results = []
+        while len(queue) != 0:
+            current = queue.pop(0)
+            results.append(current.value)
+            if current.left:
+                queue.append(current.left)
+            if current.right:
+                queue.append(current.right) 
+        return results
+
+
+
+
+
+
+my_tree = BinarySearchTree()
+my_tree.insert(47)
+my_tree.insert(21)
+my_tree.insert(76)
+my_tree.insert(18)
+my_tree.insert(27)
+my_tree.insert(52)
+my_tree.insert(82)
+
+print(my_tree.BFS())
+
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    [47, 21, 76, 18, 27, 52, 82]
+
+ """
+
+
+
+
+
+                
+
+
+
+ 
